84246311a0184030f14014b2a0880b0322c1a163
Erick Taco <erick.taco@ceer.ec>
2025-09-15 15:25:17 -0500

    15/09/2025-15:25

--- segunda-aplicacion/src/App.js
+++ segunda-aplicacion/src/App.js
@@ -1,13 +1,28 @@
 import './App.css';
-
+import React,{useState} from 'react';
 function App() {
+   //Definir el estado para almacenar el resultado de la suma
  
+   const [resultado,setResultado]=useState(null);
+  
+
+
+
+
   const elemento = <h1 className="textCenter" >Hola Mundo  </h1>
-  const elemento2=suma(5,5)
+
+
+
+  function botonPulsado(){
+    const result=suma(8,5);
+    setResultado(result)
+  }
+
 
   return <div>
-    <div>{elemento}</div>
-    <div>{elemento2}</div>
+      <button onClick={botonPulsado} style={{marginTop:"10px",marginLeft:"10px"}}>Pulsame</button>
+      <div>{elemento}</div>
+      <div >{resultado}</div>
     
     
     </div >
@@ -16,5 +31,7 @@ function App() {
 function suma(a,b){
 return a+b
 }
+
+
 
 export default App;
